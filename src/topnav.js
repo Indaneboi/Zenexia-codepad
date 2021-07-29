1a0f4bce3a99a3487c6f74291d541f2306d8c84e
Indaneboi <56091800+Indaneboi@users.noreply.github.com>
2021-07-30 03:29:58 +0530

    Update topnav.js

--- src/topnav.js
+++ src/topnav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import logo from "./images/logo.png";
-import { faCloud, faCog, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCloud} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function TopNavigation() {
